1bd4848d434afadb70b5a37efe69a8dad2b746da
KendrickNguyen76 <kendricknguyen76@gmail.com>
2023-04-26 12:16:54 -0700

    Added comments to main.rb

--- v1/main.rb
+++ v1/main.rb
@@ -1,6 +1,12 @@
 # main thing to run the to-do app in the terminal. Not much here yet, but we'll see
 # what happens lmao.
 
+# Note to self, if you want to run this, do ruby v1/main.rb
+# The require statements have been adjusted so that it works this way
+# In order to make it run while in the v1 folder, I would have to change
+# it just ./<file_name>, but i already just made the change so i'm too 
+# lazy to change it back lmao.
+
 # Constants
 UI_PATH = "./v1/to_do_ui.rb"
 
