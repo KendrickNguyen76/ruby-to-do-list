a2d55b666fbf960eee083b362eeafa55073bdacc
KendrickNguyen76 <kendricknguyen76@gmail.com>
2023-06-17 21:31:34 -0700

    Added two new files for program

---
+++ v2/program_logic.rb
@@ -0,0 +1,2 @@
+# This file contains ProgramLogic, which is responsible for
+# handling the logical bits of the to-do app.
---
+++ v2/program_ui.rb
@@ -0,0 +1,2 @@
+# This file contains the ProgramUI class, which is responsible
+# for displaying the to-do app in the terminal.
