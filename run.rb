091e6873dd8f1b5d039b095db0ae09f1f8c5ac6f
KendrickNguyen76 <kendricknguyen76@gmail.com>
2024-01-08 18:40:24 -0800

    Reverted test for testing

--- run.rb
+++ run.rb
@@ -2,7 +2,7 @@
 # I can link whatever version of the to-do app I want to
 # run to this file. This makes it easier on me so that I
 # don't have to constantly move into different directories
-# When trying to test
+# When trying to test.
 
 # Constants
 CURRENT_RUNNING_VERSION = "./v2/main.rb"
